351c9f61a4cbe950a7259ad900088629f83465ef
GAF E-TECH <gafetech229@gmail.com>
2026-07-03 11:47:50 +0200

    update site

--- script.js
+++ script.js
@@ -16,3 +16,14 @@ Besoin: ${besoin}`;
 
     window.open(url, "_blank");
 }
+function toggleSection(id) {
+
+    const section = document.getElementById(id);
+
+    if (section.style.display === "block") {
+        section.style.display = "none";
+    } else {
+        section.style.display = "block";
+    }
+
+}
